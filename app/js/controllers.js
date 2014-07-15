37f65e49e5ffb7965c02d7a12ab3e8d8e6c3c82a
Malcolm Young <malcolm.young@capgemini.com>
2014-07-15 10:09:52 +0100

    keep score

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,11 +3,28 @@
 /* Controllers */
 
 angular.module('flashcardsApp.controllers', [])
-    .controller('flashcardCtrl', ['$scope', function ($scope) {
-        $scope.card = {
-            'question': 'Nexus Sausage',
-            'answer': 'Fast just got faster with Nexus S.'};
-    }])
-    .controller('MyCtrl2', ['$scope', function ($scope) {
+  .controller('flashcardCtrl', [
+    '$scope', function ($scope) {
+      $scope.card = {
+        'question': 'Nexus Sausage',
+        'answer': 'Fast just got faster with Nexus S.'
+      };
 
-    }]);
+      $scope.score = $scope.attempts = 0;
+      $scope.scorePct = 100;
+
+      $scope.answerCorrect = function () {
+        $scope.score++;
+        $scope.attempt();
+      }
+
+      $scope.answerWrong = function () {
+        $scope.attempt();
+      }
+
+      $scope.attempt = function () {
+        $scope.attempts++;
+        $scope.scorePct = ($scope.score / $scope.attempts) * 100;
+      }
+    }
+  ]);
